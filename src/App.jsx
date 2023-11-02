f9eb5b6497347b913a017ca57ba8062481edfd33
Viktoria <vizu@html24.net>
2023-11-02 20:58:51 +0100

    adding new post + verification

--- src/App.jsx
+++ src/App.jsx
@@ -9,36 +9,82 @@ export const App = () => {
 
   const [posts, setPosts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState('');
+  const [error, setError] = useState(null);
+  const [newPost, setNewPost] = useState('');
+  const [message, setMessage] = useState('');
+  
+
+  const fetchPosts = async () => {
+    try {
+        const res = await fetch('https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts');
+        const data = await res.json();
+        if(!res.ok){
+          throw new Error('💥 Something went wrong 😱')
+        }
+        console.log(data);
+        setPosts(data);
+    }
+    catch(err){
+      console.error(err.message);
+      setError(err.message)
+    }
+    finally {
+      setIsLoading(false)
+    }
+  }
+
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-          const res = await fetch('https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts');
-          const data = await res.json();
-          if(!res.ok){
-            throw new Error('💥 Something went wrong 😱')
-          }
-          console.log(data);
-          setPosts(data);
-      }
-      catch(err){
-        console.error(err.message);
-        setError(err.message)
-      }
-      finally {
-        setIsLoading(false)
-      }
-    }
     fetchPosts();
 
   },[])
 
+  const handleNewPostChange = (event) => {
+    if (newPost.length > 140) {
+      setMessage('Your message is too long 😔');
+    } else {
+      setMessage('');
+    } 
+    
+    setNewPost(event.target.value);
+  };
+
+  const onFormSubmit = async (event) => {
+
+    event.preventDefault();
+
+    if (newPost.length < 6) {
+      setMessage('Your message is too short, it needs at least 5 letters 😔');
+      return;
+    } else {
+      setMessage('');
+    } 
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        "message": newPost
+      })
+    };
+    try {
+      const response = await fetch('https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts', options);
+      if (response.ok) {
+        await fetchPosts();
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setNewPost("");
+    }
+  }
 
   return (
     <Container>
       <Header/>
-      <Form/>
+      <Form onNewPostChange={handleNewPostChange} onFormSubmit={onFormSubmit} message={message} newPost={newPost}/>
       {isLoading && <Loader/>}
       <Posts posts={posts}/>
     </Container>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import styles from './Form.module.css';
 
-const Form = () => {
+const Form = ({newPost, onNewPostChange, onFormSubmit, message}) => {
+
+  console.log(newPost)
+
   return (
     <div className={styles.postform}>
         <h2>What is making you happy right now?</h2>
-        <form>
-            <textarea rows="3" placeholder="'If music be the food of love, play on.' – William Shakespeare"></textarea>
-            <div className={styles.postform__info}><p className={styles.postform__error}></p><p className={styles.postform__length}>0/140</p></div>
-            <button type="submit" id="submitPostBtn" aria-label="button for submiting your post"><span className={styles.postform__emoji} aria-label="heart emoji">❤️</span> Send Happy Thought <span className={styles.postform__emoji} aria-label="heart emoji">❤️</span></button>
+        <form onSubmit={onFormSubmit}>
+            <textarea 
+              rows="3" 
+              placeholder="'If music be the food of love, play on.' – William Shakespeare"
+              value={newPost}
+              onChange={onNewPostChange}
+            ></textarea>
+            <div className={styles.postform__info}>
+              <p className={styles.postform__error}>{message && message}</p>
+              <p className={styles.postform__length}>{ newPost ? newPost.length : 0}/140</p>
+            </div>
+            <button type="submit" id="submitPostBtn" aria-label="button for submiting your post" disabled={message}>
+              <span className={styles.postform__emoji} aria-label="heart emoji">❤️</span> Send Happy Thought <span className={styles.postform__emoji} aria-label="heart emoji">❤️</span>
+            </button>
         </form>
     </div>
   )
